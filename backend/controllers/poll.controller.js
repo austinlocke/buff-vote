7b7d985c2cdae0ef2864781d964fb10ce167ad9b
jdmoore5 <jdmoore5@aol.com>
2019-03-05 21:20:07 -0600

    Added routes and CRUD functions for Polls

--- backend/controllers/poll.controller.js
+++ backend/controllers/poll.controller.js
@@ -31,3 +31,79 @@ exports.createPoll = (req, res) => {
       })
   });
 };
+
+// Find a single poll with a pollId
+exports.findOnePoll = (req, res) => {
+  Poll.findById(req.params.pollId)
+  .then(poll => {
+      if(!poll) {
+          res.status(404).send({
+              message: "Error: Poll not found with id " + req.params.pollId
+          });
+      }
+      res.send(poll);
+  }).catch(err => {
+      res.status(500).send({
+          message: "Error: Could not retrieve poll with id " + req.params.pollId
+      });
+  });
+};
+
+// Retrieve and return all Polls from the database.
+exports.findAllPoll = (req, res) => {
+  Poll.find()
+  .then(poll => {
+      res.send(poll);
+  }).catch(err => {
+      res.status(500).send({
+          message: "Error occurred while retrieving all Polls."
+      });
+  });
+};
+
+
+// Find Poll using pollId and update it with the request body
+exports.updatePoll = (req, res) => {
+  Poll.findByIdAndUpdate(req.params.pollId, {
+    title: req.body.title,
+    owner: req.body.owner,
+    access_type: {
+      student: req.body.access_type.student || false,
+      faculty: req.body.access_type.faculty || false,
+      instructor: req.body.access_type.instructor || false
+    },
+    questions: req.body.questions,
+    date_created: req.body.date_created,
+    end_date: req.body.end_date,
+  }, {new: true})
+  .then(poll => {
+      if(!poll) {
+          return res.status(404).send({
+              message: "Error: Poll not found with id " + req.params.pollId
+          });
+      }
+      res.send(poll);
+  }).catch(err => {
+      return res.status(500).send({
+          message: "Error updating poll with id " + req.params.pollId
+      });
+  });
+};
+
+
+// Delete a poll with the specified Id in the request
+exports.deletePoll = (req, res) => {
+  Poll.findByIdAndDelete(req.params.pollId)
+  .then(poll => {
+      if(!poll) {
+          res.status(404).send({
+              message: "Poll not found with id " + req.params.pollId
+          });
+      }
+      res.send({message: "Poll deleted successfully!"});
+  }).catch(err => {
+      res.status(500).send({
+          message: "Could not delete Poll with id " + req.params.pollId
+      });
+  });
+};
--- backend/routes/poll.routes.js
+++ backend/routes/poll.routes.js
@@ -3,5 +3,17 @@ module.exports = (app) => {
 
   // Create a new Poll
   app.post('/api/createPoll', poll.createPoll);
+
+  // Retrieve a single poll with pollId
+  app.get('/api/findOnePoll/:pollId', poll.findOnePoll);
+
+  // Retrieve all Polls
+  app.get('/api/findAllPoll', poll.findAllPoll);
+
+  // Update a Poll with pollId
+  app.put('/api/updatePoll/:pollId', poll.updatePoll);
+
+  // Delete a Poll by passing poll _id
+  app.delete('/api/deletePoll/:pollId', poll.deletePoll);
 };
 
